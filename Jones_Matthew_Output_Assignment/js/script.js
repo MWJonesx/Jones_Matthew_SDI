7ea7d9c97491443100d7f9218dc6370f845e59f8
Matthew Jones <mwjones@fullsail.edu>
2015-07-09 23:16:25 -0400

    Number, String, Boolean, Array, Array Access Notation (fixed)

--- Jones_Matthew_Output_Assignment/js/script.js
+++ Jones_Matthew_Output_Assignment/js/script.js
@@ -17,4 +17,4 @@ console.log(namesOfPeopleAndPetsInMyHouse); // This line of code allows the user
 
 
 var namesOfPeopleAndPetsInMyHouse = ["Matt", "Amanda", "Sora", "Nessa"]; // This line of code defines and declares the array namesOfPeopleAndPetsInMyHouse.
-+console.log(namesOfPeopleAndPetsInMyHouse[2]); // This line of code uses Array Access Notation to only display the variable with the index number 2, which is Sora, on a browser's console.
+console.log(namesOfPeopleAndPetsInMyHouse[2]); // This line of code uses Array Access Notation to only display the variable with the index number 2, which is Sora, on a browser's console.
