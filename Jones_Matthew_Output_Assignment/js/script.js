7fb944cecc77ce6e13d9c1e87ce22ec43954ba6c
Matthew Jones <mwjones@fullsail.edu>
2015-07-09 23:15:33 -0400

    Number, String, Boolean, Array, Array Access Notation

--- Jones_Matthew_Output_Assignment/js/script.js
+++ Jones_Matthew_Output_Assignment/js/script.js
@@ -1,4 +1,20 @@
 //Matthew Jones Scalable Data Infrastructures C201507 Output Assignment
 
+var yearsIHaveLivedInPennsylvania = 3.5; // This line of code represents a number variable being defined and declared.
+console.log(yearsIHaveLivedInPennsylvania); // This line of code is what allows the user to view the value of yearsIHaveLivedInPennsylvania.
+
+
+var dateIGotMarried = "I got married on May 16, 2014"; //This line of code represents the variable dateIGotMarried being declared and also defined as "I got married on May 16, 2014"
+console.log(dateIGotMarried); // This line of code allows for the value of dateIGotMarried to be displayed in a browser's console.
+
+
+var iHaveTwoCats = true; // This line of code represents a boolean variable being defined and declared.
+console.log(iHaveTwoCats); //This line of code is what allows the boolean variable's value to be seen in a browser's console.
+
+
 var namesOfPeopleAndPetsInMyHouse = ["Matt", "Amanda", "Sora", "Nessa"]; // This line of code defines and declares the array namesOfPeopleAndPetsInMyHouse.
-console.log(namesOfPeopleAndPetsInMyHouse[2]); // This line of code uses Array Access Notation to only display the variable with the index number 2, which is Sora, on a browser's console.
+console.log(namesOfPeopleAndPetsInMyHouse); // This line of code allows the user to view the value of namesOfPeopleAndPetsInMyHouse on a browser's console.
+
+
+var namesOfPeopleAndPetsInMyHouse = ["Matt", "Amanda", "Sora", "Nessa"]; // This line of code defines and declares the array namesOfPeopleAndPetsInMyHouse.
++console.log(namesOfPeopleAndPetsInMyHouse[2]); // This line of code uses Array Access Notation to only display the variable with the index number 2, which is Sora, on a browser's console.
