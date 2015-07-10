1d044c9be839e9aa42b41fe27c09a2ec3a280b14
Matthew Jones <mwjones@fullsail.edu>
2015-07-09 22:37:06 -0400

    Boolean Variable

--- Jones_Matthew_Output_Assignment/js/script.js
+++ Jones_Matthew_Output_Assignment/js/script.js
@@ -1,2 +1,2 @@
-var dateIGotMarried = "I got married on May 16, 2014"; //This line of code represents the variable dateIGotMarried being declared and also defined as "I got married on May 16, 2014"
-console.log(dateIGotMarried); // This line of code allows for the value of dateIGotMarried to be displayed in a browser's console.
+var iHaveTwoCats = true; // This line of code represents a boolean variable being defined and declared.
+console.log(iHaveTwoCats); //This line of code is what allows the boolean variable's value to be seen in a browser's console.
