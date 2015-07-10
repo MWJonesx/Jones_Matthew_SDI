b81def2ae0b1d816bcf33809153a3c63a3bd9b8d
Matthew Jones <mwjones@fullsail.edu>
2015-07-09 22:25:05 -0400

    String Variable

--- Jones_Matthew_Output_Assignment/js/script.js
+++ Jones_Matthew_Output_Assignment/js/script.js
@@ -1,1 +1,2 @@
-alert("test");
+var dateIGotMarried = "I got married on May 16, 2014"; //This line of code represents the variable dateIGotMarried being declared and also defined as "I got married on May 16, 2014"
+console.log(dateIGotMarried); // This line of code allows for the value of dateIGotMarried to be displayed in a browser's console.
