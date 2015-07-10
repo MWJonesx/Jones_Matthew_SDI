70a5bdd6f466c5f2334f0c45560531fbbfc3b706
Matthew Jones <mwjones@fullsail.edu>
2015-07-09 22:46:23 -0400

    Number Variable w/ name,class,term, and assignment

--- Jones_Matthew_Output_Assignment/js/script.js
+++ Jones_Matthew_Output_Assignment/js/script.js
@@ -1,2 +1,4 @@
+//Matthew Jones Scalable Data Infrastructures C201507 Output Assignment
+
 var yearsIHaveLivedInPennsylvania = 3.5; // This line of code represents a number variable being defined and declared.
 console.log(yearsIHaveLivedInPennsylvania); // This line of code is what allows the user to view the value of yearsIHaveLivedInPennsylvania.
