63203a97e85f4e488e356bc93a8343c40e0575c7
Matthew Jones <mwjones@fullsail.edu>
2015-07-09 22:48:18 -0400

    String Variable w/ name, class, term, and assignment

--- Jones_Matthew_Output_Assignment/js/script.js
+++ Jones_Matthew_Output_Assignment/js/script.js
@@ -1,4 +1,4 @@
 //Matthew Jones Scalable Data Infrastructures C201507 Output Assignment
 
-var yearsIHaveLivedInPennsylvania = 3.5; // This line of code represents a number variable being defined and declared.
-console.log(yearsIHaveLivedInPennsylvania); // This line of code is what allows the user to view the value of yearsIHaveLivedInPennsylvania.
+var dateIGotMarried = "I got married on May 16, 2014"; //This line of code represents the variable dateIGotMarried being declared and also defined as "I got married on May 16, 2014"
+console.log(dateIGotMarried); // This line of code allows for the value of dateIGotMarried to be displayed in a browser's console.
