0e0fee5a757c76cc6b7887e93edc0e4a84b788ac
Matthew Jones <mwjones@fullsail.edu>
2015-07-09 22:42:53 -0400

    Number Variable

--- Jones_Matthew_Output_Assignment/js/script.js
+++ Jones_Matthew_Output_Assignment/js/script.js
@@ -1,2 +1,2 @@
-var iHaveTwoCats = true; // This line of code represents a boolean variable being defined and declared.
-console.log(iHaveTwoCats); //This line of code is what allows the boolean variable's value to be seen in a browser's console.
+var yearsIHaveLivedInPennsylvania = 3.5; // This line of code represents a number variable being defined and declared.
+console.log(yearsIHaveLivedInPennsylvania); // This line of code is what allows the user to view the value of yearsIHaveLivedInPennsylvania.
