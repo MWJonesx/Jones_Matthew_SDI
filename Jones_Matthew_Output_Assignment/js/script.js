254a2d5346b6b69eeb71d50134dd79a2e5682b01
Matthew Jones <mwjones@fullsail.edu>
2015-07-09 22:54:30 -0400

    Array

--- Jones_Matthew_Output_Assignment/js/script.js
+++ Jones_Matthew_Output_Assignment/js/script.js
@@ -1,4 +1,4 @@
 //Matthew Jones Scalable Data Infrastructures C201507 Output Assignment
 
-var iHaveTwoCats = true; // This line of code represents a boolean variable being defined and declared.
-console.log(iHaveTwoCats); //This line of code is what allows the boolean variable's value to be seen in a browser's console.
+var namesOfPeopleAndPetsInMyHouse = ["Matt, Amanda, Sora, Nessa"];
+console.log(namesOfPeopleAndPetsInMyHouse);
