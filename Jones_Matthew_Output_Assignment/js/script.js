17bee03a95107224dacf31e6fd3e7514e47d448d
Matthew Jones <mwjones@fullsail.edu>
2015-07-09 23:01:31 -0400

    Array Access Notation w/ comments

--- Jones_Matthew_Output_Assignment/js/script.js
+++ Jones_Matthew_Output_Assignment/js/script.js
@@ -1,4 +1,4 @@
 //Matthew Jones Scalable Data Infrastructures C201507 Output Assignment
 
-var namesOfPeopleAndPetsInMyHouse = ["Matt, Amanda, Sora, Nessa"]; // This line of code defines and declares the array namesOfPeopleAndPetsInMyHouse.
-console.log(namesOfPeopleAndPetsInMyHouse); // This line of code allows the user to view the value of namesOfPeopleAndPetsInMyHouse on a browser's console.
+var namesOfPeopleAndPetsInMyHouse = ["Matt", "Amanda", "Sora", "Nessa"]; // This line of code defines and declares the array namesOfPeopleAndPetsInMyHouse.
+console.log(namesOfPeopleAndPetsInMyHouse[2]); // This line of code uses Array Access Notation to only display the variable with the index number 2, which is Sora, on a browser's console.
