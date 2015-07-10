14316f762e001c5f437c4db5364444f373707060
Matthew Jones <mwjones@fullsail.edu>
2015-07-09 22:57:08 -0400

    Array w/ comments

--- Jones_Matthew_Output_Assignment/js/script.js
+++ Jones_Matthew_Output_Assignment/js/script.js
@@ -1,4 +1,4 @@
 //Matthew Jones Scalable Data Infrastructures C201507 Output Assignment
 
-var namesOfPeopleAndPetsInMyHouse = ["Matt, Amanda, Sora, Nessa"];
-console.log(namesOfPeopleAndPetsInMyHouse);
+var namesOfPeopleAndPetsInMyHouse = ["Matt, Amanda, Sora, Nessa"]; // This line of code defines and declares the array namesOfPeopleAndPetsInMyHouse.
+console.log(namesOfPeopleAndPetsInMyHouse); // This line of code allows the user to view the value of namesOfPeopleAndPetsInMyHouse on a browser's console.
