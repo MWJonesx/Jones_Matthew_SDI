3eb6656580b594df9faee7c66feeee3fcdaffb2a
Matthew Jones <mwjones@fullsail.edu>
2015-07-16 21:25:59 -0400

    First item in array prompted

--- Jones_Matthew_Expressions_Assignment/js/script.js
+++ Jones_Matthew_Expressions_Assignment/js/script.js
@@ -3,3 +3,15 @@
  */
 
 //alert("Good job! JavaScript works! "); //Testing my JavaScript file connection
+
+//variables
+
+var purchaseItems = [];
+var arrayIndex = 0;
+const salesTax = 0.06;
+var purchaseTotal;
+
+purchaseItems[arrayIndex] = prompt("How much does the red shirt cost?"); //Prompting the user to enter the cost of a red shirt.
+console.log(arrayIndex); // This prints out the current value of arrayIndex
+console.log("purchaseItems[0]="  + purchaseItems[arrayIndex]); //This outputs the first value of the array
+
