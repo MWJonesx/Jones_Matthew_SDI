e61846cd213ec8cc13e88429586a799bb80f7329
Matthew Jones <mwjones@fullsail.edu>
2015-07-16 22:40:18 -0400

    array and array index notation

--- Jones_Matthew_Expressions_Assignment/js/script.js
+++ Jones_Matthew_Expressions_Assignment/js/script.js
@@ -16,4 +16,6 @@ var redShirt = prompt("How much does the red shirt cost?"); // This prompts the 
 var blueShirt = prompt("How much does the blue shirt cost?");// This line prompts the user to input the value for the variable of blueShirt
 var purpleShirt = prompt("How much does the purple shirt cost?"); // This line prompts the user to input the value for the variable of purpleShirt
 var tealShirt = prompt("how much does the teal shirt cost?"); // This line prompts the user to input the value for the variable of tealShirt
+var shoppingItems=[redShirt,blueShirt, purpleShirt, tealShirt]; // This line shows the array of the four variables
+var totalPrice = Number(shoppingItems[0]) + Number(shoppingItems[1]) + Number(shoppingItems[2]) + Number(shoppingItems[3]); //This line shows the use of Array Index Notation
 
