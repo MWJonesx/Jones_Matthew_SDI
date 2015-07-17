6555999584f2e65ac59eef476d20151f96c44e1d
Matthew Jones <mwjones@fullsail.edu>
2015-07-16 21:29:40 -0400

    Commented on what lines 9-12 are for.

--- Jones_Matthew_Expressions_Assignment/js/script.js
+++ Jones_Matthew_Expressions_Assignment/js/script.js
@@ -6,10 +6,10 @@
 
 //variables
 
-var purchaseItems = [];
-var arrayIndex = 0;
-const salesTax = 0.06;
-var purchaseTotal;
+var purchaseItems = []; //This line indicates the array
+var arrayIndex = 0; // This line defines the value of arrayIndex as 0
+const salesTax = 0.06; // This is the constant salesTax to be calculated into the total
+var purchaseTotal; // This line declares the variable purchaseTotal
 
 purchaseItems[arrayIndex] = prompt("How much does the red shirt cost?"); //Prompting the user to enter the cost of a red shirt.
 console.log(arrayIndex); // This prints out the current value of arrayIndex
