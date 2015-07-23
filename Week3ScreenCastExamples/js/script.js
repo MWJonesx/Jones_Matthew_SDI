8430886f814d55cc97c406ff1fe70341c0121c67
Matthew Jones <mwjones@fullsail.edu>
2015-07-23 12:58:05 -0400

    Nesting conditionals

--- Week3ScreenCastExamples/js/script.js
+++ Week3ScreenCastExamples/js/script.js
@@ -5,46 +5,15 @@
 
 //alert("JavaScript works!");
 
-//Basic "skeleton" for conditional statements
 
-//if(condition){
-//    actions();
-//} else {
-//    actions();
-//}
-//
-////Example of how to use a conditional statement
-//
-//sunny = true;
-//
-//if(sunny){
-//    goToTheBeach();
-//} else {
-//    goToTheMovies();
-//}
+//Nesting conditionals
+//Nesting conditionals are used when there is an extra step within a conditional statement
 
-
-//Using conditional operators
-// > greater than
-// < less than
-// >= greater than or equal to
-// <== less than or equal to
-// != not equal to
-// == exactly equal to
-
-
-temp = 82;
-
-if(temp>70) {           // This block of code means that if the temperature is greater than 70 degrees we will go to the beach but if not then we will go to the movies
+if(sunny){
     goToTheBeach();
-} else {
-    goToTheMovies();
-}
-
-temp = 82
-
-if (temp == 75) {      // This block of code means that if the temperature is EXACTLY 75 degrees we will go to the beach but if not then we will go to the movies.
-    goToTheBeach();
+    if(warmWater){            //This block of code means that if it sunny outside AND the water is warm then we should wear our new swim suit
+        wearNewSuit();
+    }
 } else {
     goToTheMovies();
 }
