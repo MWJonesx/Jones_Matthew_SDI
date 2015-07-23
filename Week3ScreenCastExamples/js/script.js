df5e5b2f9dcf166007977157b7b1dc55828975f8
Matthew Jones <mwjones@fullsail.edu>
2015-07-23 14:16:41 -0400

    else if

--- Week3ScreenCastExamples/js/script.js
+++ Week3ScreenCastExamples/js/script.js
@@ -4,38 +4,29 @@
 //Examples from week 3 screen cast videos
 
 
-//Conditional Logic
+
 
 //alert("JavaScript works!");
-
+//Conditional Logic - else if
 
 // if a child is tall enough to ride a roller coaster
 
+
 var kidHeight = 47;
 var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
 
 // if the child is tall enough, print out to the console "You can ride the coaster!"
 // if the kid is over 48 inches in height
 //if the kid is wearing shoes that give him 2 extra inches in height
 
 if(kidHeight > minHeight){
-    //code performed if the condition is true
+    //you can ride!
     console.log("You can ride the coaster!");
-}
-if(kidHeight <= minHeight){
-    console.log("Sorry kid, you've got some growing to do first!");
-}
-
-var kidHeight = 47;
-var minHeight = 48;
-
-// if the child is tall enough, print out to the console "You can ride the coaster!"
-// if the kid is over 48 inches in height
-//if the kid is wearing shoes that give him 2 extra inches in height
-
-if(kidHeight > minHeight){
-    //code performed if the condition is true
-    console.log("You can ride the coaster!");
-} else {
+} else if(kidHeight > wParentHeight) {
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
+}else{
+    //sorry you have growing to do
     console.log("Sorry kid, you've got some growing to do first!");
 }
