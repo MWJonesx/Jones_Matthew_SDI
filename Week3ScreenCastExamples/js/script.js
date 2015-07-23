50de5d0ab4732ed422ea666e05368c645e962a1a
Matthew Jones <mwjones@fullsail.edu>
2015-07-23 13:09:48 -0400

    Conditional Logic

--- Week3ScreenCastExamples/js/script.js
+++ Week3ScreenCastExamples/js/script.js
@@ -3,17 +3,19 @@
  */
 //Examples from week 3 screen cast videos
 
+
+//Conditional Logic
+
 //alert("JavaScript works!");
 
 
-//Nesting conditionals
-//Nesting conditionals are used when there is an extra step within a conditional statement
+// if a child is old enough to ride a roller coaster
 
-if(sunny){
-    goToTheBeach();
-    if(warmWater){            //This block of code means that if it sunny outside AND the water is warm then we should wear our new swim suit
-        wearNewSuit();
-    }
-} else {
-    goToTheMovies();
+var oldEnough = true;
+
+// if the child is old enough, print ot the console "You can ride the coaster!"
+
+if(oldEnough){
+    //code performed if the condition is true
+    console.log("You can ride the coaster!");
 }
