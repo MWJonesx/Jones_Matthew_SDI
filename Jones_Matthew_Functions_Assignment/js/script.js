d81acac2eb6c290b3d43507b8697e7ac90de532e
Matthew Jones <mwjones@fullsail.edu>
2015-07-30 21:04:12 -0400

    prompt validation

--- Jones_Matthew_Functions_Assignment/js/script.js
+++ Jones_Matthew_Functions_Assignment/js/script.js
@@ -13,10 +13,22 @@
 var powerBallNums //this is the variable that will be used to output the 5 random numbers of the PowerBall lottery
 var lotto //this is the variable that will be used to output the 6 random numbers for the Florida State Lottery
 var powerBall //this is the variable that will be used to output the 1 random number that will represent the PowerBall in the PowerBall lottery
-var game = prompt("Would you like to play the PowerBall or the Florida State Lottery?"); //this will prompt the user to tell which game they would like to play
+var game = prompt("Would you like to play the PowerBall or the Florida State Lottery? (case sensitive)"); //this will prompt the user to tell which game they would like to play
 
 
 //functions
+function gameValidation(ballOrLotto) { //this sets up the function gameValidation that will prompt the user to tell which game they would like to play if they do not enter anything
+
+
+    while (ballOrLotto === "") { //this sets up the while loop. This line essentially say while the user enters nothing when asked then do that next line of code
+
+        ballOrLotto = prompt("If you really want to play you must choose, PowerBall or Florida State Lottery? (case sensitive"); //this is the prompt that the user will see if they enter nothing
+
+    }
+    return ballOrLotto; //this returns the value to gameValidation
+}
 
 
 //main code
+game = gameValidation(game); //this changes the value of game to be whatever the user inputs when prompted in the above code
+console.log("You have chosen to play " + game + "!"); //this is the output that will appear once the user enters which game they would like to play
