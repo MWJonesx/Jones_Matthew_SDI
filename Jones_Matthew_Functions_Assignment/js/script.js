4a9342e014682918d88e29c3c1af19a0878c6fee
Matthew Jones <mwjones@fullsail.edu>
2015-07-30 20:50:58 -0400

    Stub code

--- Jones_Matthew_Functions_Assignment/js/script.js
+++ Jones_Matthew_Functions_Assignment/js/script.js
@@ -5,4 +5,15 @@
  * 201507
  */
 
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//Determine which lottery the user wants to play and output the correct amount of random numbers
+
+//variables
+
+
+
+//functions
+
+
+//main code
