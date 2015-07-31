2730edcef75708bde6e20ee0562b4a05d7bc37c6
Matthew Jones <mwjones@fullsail.edu>
2015-07-30 20:55:36 -0400

    Variables

--- Jones_Matthew_Functions_Assignment/js/script.js
+++ Jones_Matthew_Functions_Assignment/js/script.js
@@ -10,7 +10,10 @@
 //Determine which lottery the user wants to play and output the correct amount of random numbers
 
 //variables
-
+var powerBallNums //this is the variable that will be used to output the 5 random numbers of the PowerBall lottery
+var lotto //this is the variable that will be used to output the 6 random numbers for the Florida State Lottery
+var powerBall //this is the variable that will be used to output the 1 random number that will represent the PowerBall in the PowerBall lottery
+var game = prompt("Would you like to play the PowerBall or the Florida State Lottery?"); //this will prompt the user to tell which game they would like to play
 
 
 //functions
